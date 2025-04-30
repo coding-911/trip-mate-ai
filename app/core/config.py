e03e2243d8b87d8626ab64c421425666d6de7f3a
coding-911 <ye22im@gmail.com>
2025-04-30 21:30:59 +0900

    feat: User Event 로그 기능 추가 (Elasticsearch)

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ELASTICSEARCH_HOSTS: str
     ELASTIC_USERNAME: str
     ELASTIC_PASSWORD: str
     KAKAO_REST_API_KEY: str
